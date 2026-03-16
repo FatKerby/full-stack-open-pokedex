29a9abaf1cf8acaf1e3b1a543eee2d276b6d6e08
Kyle <242663018+FatKerby@users.noreply.github.com>
2026-03-15 22:18:04 -0400

    11.9

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -7,3 +7,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
+
+describe('Pokedex', () => {
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.getByText('overgrown')).toBeVisible()
+  })
+})

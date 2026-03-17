7f08e15c7f87b3909dbca439eed99ea57278bf15
Kyle <242663018+FatKerby@users.noreply.github.com>
2026-03-17 03:35:19 -0400

    fixed port

--- playwright.config.js
+++ playwright.config.js
@@ -27,14 +27,14 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://localhost:5001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace: 'on-first-retry',
   },
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:8080',
+    url: 'http://localhost:5001',
     reuseExistingServer: !process.env.CI,
   }
 
